de9ca8f7d6f5726b9126420317963b96d77fa682
Tonalli López <contact@tuentyfaiv.com>
2024-01-14 11:59:05 -0600

    🚧 REFACTOR: Add simple schemas and update form context types

--- packages/form/src/lib/logic/typing/globals/contexts.ts
+++ packages/form/src/lib/logic/typing/globals/contexts.ts
@@ -1,9 +1,10 @@
 import type { Writable } from "svelte/store";
 import type { Errors } from "../utils/errors.js";
 import type {
+  Fields,
   Submit,
   SubmitAction,
-  SubmitOptions,
+  SubmitConfig,
 } from "../stores/form.js";
 import type {
   ErrorsStyles,
@@ -13,26 +14,33 @@ import type {
   OptionStyles,
   SelectStyles,
 } from "./styles.js";
+import type { UserEvent } from "./types.js";
 
 export interface ContextStyles {
   /** Set to true to replace the default styles  @default false */
   replace?: boolean;
+  /** Styles for the field component */
   field?: FieldStyles;
+  /** Styles for the file component */
   file?: FileStyles;
+  /** Styles for the select component */
   select?: SelectStyles;
+  /** Styles for the option component */
   option?: OptionStyles;
+  /** Component icons paths */
   icons?: Icons | null;
+  /** Styles for the errors component */
   errors?: ErrorsStyles;
 }
 
-export interface ContextForm<Values, Fields> {
+export interface ContextForm<Values extends Fields, Keys extends keyof Values = keyof Values> {
   loading: Writable<boolean>;
-  errors: Writable<Errors>;
+  errors: Writable<Errors<Values>>;
   data: Writable<Values>;
-  reset(resetErrors?: boolean): void;
-  setError(name: Fields, error?: unknown): void;
-  setField(field: Fields, value: unknown, validate?: boolean): Promise<void>;
-  check(event: FocusEvent | Event): Promise<void>;
-  submit<T extends Values>(action: SubmitAction<T>, options?: SubmitOptions): Submit;
   styles: Writable<ContextStyles>;
+  reset(clear?: boolean): void;
+  setError(field: Keys, error?: string | null): Promise<void>;
+  setField(field: Keys, value: Values[Keys], validate?: boolean): Promise<void>;
+  check(event: UserEvent<HTMLInputElement | HTMLTextAreaElement, FocusEvent | Event>): Promise<void>;
+  submit<T extends Fields = Values>(action: SubmitAction<T>, config?: SubmitConfig<T>): Submit;
 }
--- packages/form/src/lib/logic/typing/stores/form.ts
+++ packages/form/src/lib/logic/typing/stores/form.ts
@@ -1,22 +1,60 @@
+import type { Writable } from "svelte/store";
 import type { ContextStyles } from "../globals/contexts.js";
+import type { UserEvent } from "../globals/types.js";
+import type { Errors } from "../utils/errors.js";
 
-export interface FormStoreConfig<T> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Fields = Record<string, any>;
+
+export type Schema = RegExp | "string" | "number" | "boolean" | "date";
+export type FieldPropSchema<T> = {
+  required ?: boolean;
+  type: T;
+  min ?: number;
+  max ?: number;
+};
+
+export type ArraySchema = FieldPropSchema<"array"> & {
+  item: Schema | FieldPropSchema<Schema> | ArraySchema | FieldsSchema;
+};
+
+export type FieldsSchema = {
+  [key: string]: Schema | FieldPropSchema<Schema> | ArraySchema | FieldsSchema;
+};
+
+export type DeepSchema = ArraySchema | FieldPropSchema<Schema> | FieldsSchema;
+
+export abstract class Adapter<T> {
+  abstract initial(): {
+    fields: T;
+    errors: Errors<T>;
+  };
+  abstract validate<D = T>(data: D): Promise<void>;
+  abstract field(field: keyof T, value: T[keyof T], errors: Writable<Errors<T>>): Promise<void>;
+  abstract errors(
+    error: unknown,
+    errors: Writable<Errors<T>>,
+    handle?: (error: unknown) => (void | Promise<void>)
+  ): Promise<void>;
+}
+
+export interface FaivFormConfig<F, T extends FieldsSchema | Adapter<F>> {
   fields: T;
   styles?: ContextStyles;
 }
 
-export type SubmitAction<T> = (values: T) => Promise<void>;
+export type Submit = (event: UserEvent<HTMLFormElement, SubmitEvent>) => Promise<void>;
+export type SubmitAction<T> = (values: T) => (Promise<void> | void);
 
-export type Submit = (event: SubmitEvent) => Promise<void>;
-
-export interface SubmitOptions {
+export interface SubmitConfig<T extends Fields> {
   error?(error: unknown): void;
   finish?: VoidFunction;
   context?: string;
   reset?: boolean;
+  initial?: T;
 }
 
-export interface FieldsErrorsConfig {
+export interface SetErrorsConfig {
   error: unknown;
-  handle?: (error: unknown) => void;
+  handle?: (error: unknown) => (void | Promise<void>);
 }
