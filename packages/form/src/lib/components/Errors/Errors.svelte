4a3be6cdc8dc1d353370c4a951e0bacadf7445b6
Tonalli López <contact@tuentyfaiv.com>
2024-10-22 20:47:46 -0600

    🚧 REFACTOR: Move errors css to file

--- packages/form/src/lib/components/Errors/Errors.svelte
+++ packages/form/src/lib/components/Errors/Errors.svelte
@@ -5,12 +5,13 @@
 
   import type { Props } from "./Errors.proptypes.js";
 
+  import "./Errors.css";
+
   type $$Props = Props;
 
   export let context: Props["context"] = "form";
   export let id: Props["id"] = null;
   export let styles: Props["styles"] = {};
-  export let datas: Props["datas"] = {};
 
   $: form = useForm(context);
   $: ({ errors, styles: ctxStyles } = $form);
@@ -21,8 +22,8 @@
   $: styls = getStyles<Exclude<Props["styles"], undefined>>({
     replace,
     internals: {
-      container: "faivform-errors-container",
-      item: "faivform-errors-item",
+      container: "svorm-errors-container",
+      item: "svorm-errors-item",
     },
     externals: {
       container: styles?.container ?? ctxErrorStyles?.container,
@@ -31,44 +32,18 @@
   });
 </script>
 
-{#if show}
-  <ul class={styls.container} {...datas} {id}>
-    {#each Object.entries($errors) as [field, error], index (`${field}-list-${index}`)}
-      {#if error !== null}
-        <li class={styls.item} transition:fade={{ duration: 200 }}>
-          <slot {error} {field}>
-            {`${field}: ${error}`}
-          </slot>
-        </li>
-      {/if}
-    {/each}
-  </ul>
-{/if}
-
-<style>
-  :global(.faivform-errors-container) {
-    box-sizing: border-box;
-    display: flex;
-    margin: var(--faivform-space) 0 0;
-    padding: 0;
-    gap: calc(var(--faivform-space) / 4);
-    list-style: none;
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex-direction: column;
-  }
-
-  :global(.faivform-errors-item) {
-    box-sizing: inherit;
-    display: block;
-    width: 100%;
-    margin: 0;
-    padding: calc(var(--faivform-space) / 2) calc(var(--faivform-space) / 1.5);
-    background-color: var(--faivform-error-color);
-    color: var(--faivform-error-text);
-    font-size: calc(var(--faivform-space) - (var(--faivform-space) / 8));
-    line-height: calc(var(--faivform-space) + (var(--faivform-space) / 8));
-    font-family: var(--faivform-placeholder-font);
-    border-radius: var(--faivform-radius);
-  }
-</style>
+{#key form}
+  {#if show}
+    <ul class={styls.container} {id}>
+      {#each Object.entries($errors) as [field, error], index (`${field}-list-${index}`)}
+        {#if error !== null}
+          <li class={styls.item} transition:fade={{ duration: 200 }}>
+            <slot {error} {field}>
+              {`${field}: ${error}`}
+            </slot>
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  {/if}
+{/key}
